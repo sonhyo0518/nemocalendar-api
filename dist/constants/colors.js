b775396655f33f774ae603ada753469540eae7a3
sonhyo0518 <sonhg0518@gmail.com>
2026-09-22 22:20:16 +0900

    chore: dist를 git 추적에서 제외

--- dist/constants/colors.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DEFAULT_ANNIV_COLOR = exports.DEFAULT_BANNER_COLOR = void 0;
-/** frontend/lib/color-presets.ts 와 값을 맞출 것 */
-exports.DEFAULT_BANNER_COLOR = '#9FE1E7';
-exports.DEFAULT_ANNIV_COLOR = '#CC79A7';
--- dist/lib/prisma.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.prisma = void 0;
-const client_1 = require("../generated/prisma/client");
-BigInt.prototype.toJSON =
-    function toJSON() {
-        return this.toString();
-    };
-exports.prisma = new client_1.PrismaClient();
